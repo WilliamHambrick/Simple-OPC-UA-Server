85c969dffb05845e56b67977e403058ccdbf1ea4
William Hambrick <103603887+WilliamHambrick@users.noreply.github.com>
2022-09-29 14:49:54 -0500

    init
    Uploading of existing files and codebase

---
+++ utility/load_config.py
@@ -0,0 +1,8 @@
+import json, os, sys
+
+def load_config():
+    if not os.path.isfile("config.json"):
+        sys.exit("'config.json' not found! Please add it and try again.")
+    else:
+        with open("config.json") as file:
+            return json.load(file)
