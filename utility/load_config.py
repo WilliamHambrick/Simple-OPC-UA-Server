2db124d906bd8b0ea3d314a766b7d3cd016fd445
William Hambrick <103603887+WilliamHambrick@users.noreply.github.com>
2022-09-29 15:58:14 -0500

    Update load_config.py

--- utility/load_config.py
+++ utility/load_config.py
@@ -1,7 +1,7 @@
 import json, os, sys
 
 def load_config():
-    if not os.path.isfile("config.json"):
+    if not os.path.isfile("./config.json"):
         sys.exit("'config.json' not found! Please add it and try again.")
     else:
         with open("config.json") as file:
