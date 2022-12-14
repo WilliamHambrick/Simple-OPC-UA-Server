88e25b88bb35f8fd55b94c031ba0a44bc7581fb7
William Hambrick <103603887+WilliamHambrick@users.noreply.github.com>
2022-12-14 08:54:19 -0600

    Test Values

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ sys.path.insert(0, "..")
 from asyncua import ua, Server
 from asyncua.common.methods import uamethod
 from utility.load_config import load_config
+import random
 
 config = load_config()
 
@@ -27,7 +28,13 @@ async def main():
     # populating our address space
     # server.nodes, contains links to very common nodes like objects and root
     myobj = await server.nodes.objects.add_object(idx, 'MyObject')
-    myvar = await myobj.add_variable(idx, 'MyVariable', 6.7)
+    myvar = await myobj.add_variable(idx, 'Runtime', 0)
+    
+    temp = await myobj.add_variable(idx, 'Temp', 0)
+    axis_x = await myobj.add_variable(idx, 'X-Axis-Pos', 0)
+    axis_y = await myobj.add_variable(idx, 'Y-Axis-Pos', 0)
+    axis_z = await myobj.add_variable(idx, 'Z-Axis-Pos', 0)
+    
     # Set MyVariable to be writable by clients
     await myvar.set_writable()
     await server.nodes.objects.add_method(ua.NodeId('ServerMethod', 2), ua.QualifiedName('ServerMethod', 2), func, [ua.VariantType.Int64], [ua.VariantType.Int64])
@@ -35,9 +42,20 @@ async def main():
     async with server:
         while True:
             await asyncio.sleep(1)
-            new_val = await myvar.get_value() + 0.1
+            new_val = await myvar.get_value() + 1
             _logger.info('Set value of %s to %.1f', myvar, new_val)
+            temp_value = random.randrange(0, 100)
+            x_value = random.randrange(0, 10)
+            _logger.info('Set value of %s to %.1f', axis_x, x_value)
+            y_value = random.randrange(0, 10)
+            _logger.info('Set value of %s to %.1f', axis_y, y_value)
+            z_value = random.randrange(0, 10)
+            _logger.info('Set value of %s to %.1f', axis_z, z_value)
             await myvar.write_value(new_val)
+            await temp.write_value(temp_value)
+            await axis_x.write_value(x_value)
+            await axis_y.write_value(y_value)
+            await axis_z.write_value(z_value)
 
 
 if __name__ == '__main__':
